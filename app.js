9724fb0d8491c2b7ac73b683ed31ca224a19c469
Brenda Nascimento <162237685+Behguedes@users.noreply.github.com>
2025-02-26 17:40:36 -0300

    commit sorteio pronto

--- app.js
+++ app.js
@@ -1,1 +1,53 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+let textoInserido = document.getElementById('amigo');
+
+
+function adicionarAmigo() {
+    if (textoInserido.value.trim()==="") {
+        alert('Por favor, insira um nome!');
+    }else {
+        if (amigos.includes(textoInserido.value.trim())) {
+      alert('Este amigo já foi adicionado!');
+    } else {
+        amigos.push(textoInserido.value);
+        console.log('amigo adicionado:', textoInserido.value);
+        console.log('lista de amigos:', amigos);
+
+        atualizarListaDeAmigos();
+    }
+    limparCampo();
+}}
+
+function limparCampo() {
+    textoInserido.value='';
+}
+
+function atualizarListaDeAmigos() {
+    const listaAmigosDiv = document.getElementById('listaAmigos'); 
+    listaAmigosDiv.innerHTML = '';
+    amigos.forEach(amigo => {
+      const li = document.createElement('li');
+      li.textContent = amigo; 
+      listaAmigosDiv.appendChild(li);
+    });
+  }
+
+function sortearAmigo() {
+    if (amigos.length === 0){
+        alert('Não há amigos para sortear!');
+    }else {
+        // Gera um índice aleatório
+        const indiceAleatorio = Math.floor(Math.random() * amigos.length);
+        // Obtém o nome sorteado
+        const amigoSorteado = amigos[indiceAleatorio];
+        const resultadoDiv = document.getElementById('resultado');
+        resultadoDiv.innerHTML = `O amigo sorteado é: ${amigoSorteado}`;
+
+        amigos = [];
+        atualizarListaDeAmigos();
+    }
+}
+
+
+adicionarAmigo();
+sortearAmigo();
